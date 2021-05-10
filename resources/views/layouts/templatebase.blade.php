34b197c261d0ad16805955233ca71479dbbfcbad
Juliana Fajardo <juliana.fajardo92@gmail.com>
2021-05-10 17:28:23 -0500

    navbar

--- resources/views/layouts/templatebase.blade.php
+++ resources/views/layouts/templatebase.blade.php
@@ -10,19 +10,45 @@
 <body>
     <header class="shadow-lg"> 
 
-        <nav class="bg-blue-700 py-6">
-            <a href="">
-            <img src="{{asset ('images') }}" class="h-8 mx-auto" alt="Logo">
-            </a>
-        </nav>
 
         @auth
             <a href="url('dashboard')" class="text-sm text-gray-700 underline ">Dashboard</a>
         @else
-            <a href="{{url('/')}}">Home</a>
-            <a href="{{url('/Nosotros')}}">Nosotros</a>
-            <a href="{{url('login')}}" class="text-sm text-gray-700 underline ">Login</a>
-            <a href="{{url('register')}}" class="ml-4 text-sm text-gray-700 underline">Registro</a>
+        <nav id="header" class="fixed w-full z-30 top-0 text-white">
+        <div class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
+            <div class="pl-4 flex items-center">
+                <a class="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="#">
+                    <!--Icon from: http://www.potlabicons.com/ -->
+                    ¡Nosotros!
+                </a>
+            </div>
+            <div class="block lg:hidden pr-4">
+                <button id="nav-toggle" class="flex items-center p-1 text-pink-800 hover:text-gray-900 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                    <svg class="fill-current h-6 w-6" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <title>Menu</title>
+                        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                    </svg>
+                </button>
+            </div>
+            <div class="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20" id="nav-content">
+                <ul class="list-reset lg:flex justify-end flex-1 items-center">
+                    <li class="mr-3">
+                    <a href="{{url('/')}}">Home</a>
+                    </li>
+                    <li class="mr-3">
+                    <a href="{{url('/Nosotros')}}">Nosotros</a>
+                    </li>
+                    <li class="mr-3">
+                    <a href="{{url('login')}}" class="text-sm text-gray-700 underline ">Login</a>
+                    </li>
+                </ul>
+                <button id="navAction" class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                <a href="{{url('register')}}" class="ml-4 text-sm text-gray-700 underline">Registro</a>
+                </button>
+            </div>
+        </div>
+        <hr class="border-b border-gray-100 opacity-25 my-0 py-0" />
+    </nav>
             
         @endif
 
@@ -40,3 +66,4 @@
         </footer>
 </body>
 </html>
+
