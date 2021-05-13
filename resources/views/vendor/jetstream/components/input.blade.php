fa289acd3524243977df8d5952c7aefae36e54fb
Poly <rodriguezpaula606@gmail.com>
2021-05-13 14:45:46 -0500

    new vendor

---
+++ resources/views/vendor/jetstream/components/input.blade.php
@@ -0,0 +1,4 @@
+@props(['disabled' => false])
+
+<!-- imputs -->
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-red-300 focus:border-red-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm']) !!}>
