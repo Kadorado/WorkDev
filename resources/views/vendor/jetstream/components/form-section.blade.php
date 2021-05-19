564404c824a9cbc7cf7d1895616149c046cb0bee
Poly <rodriguezpaula606@gmail.com>
2021-05-19 12:24:38 -0500

    datos perfil

--- resources/views/vendor/jetstream/components/form-section.blade.php
+++ resources/views/vendor/jetstream/components/form-section.blade.php
@@ -2,22 +2,22 @@
 
 
 
-<div {{ $attributes->merge(['class' => 'md:grid md:grid-cols-3 md:gap-6']) }}>
+<div {{ $attributes->merge(['class' => 'md:grid md:grid-cols-1 md:gap-6']) }}>
     <x-jet-section-title>
         <x-slot name="title">{{ $title ?? '' }}</x-slot>
         <x-slot name="description">{{ $description }}</x-slot>
     </x-jet-section-title>
 
     <div class="mt-5 md:mt-0 md:col-span-2">
-        <form class="w-full max-w-sm" wire:submit.prevent="{{ $submit }}">
-            <div class="md:flex md:items-center mb-6" {{ isset($actions) ? 'sm:rounded-tl-md sm:rounded-tr-md' : 'sm:rounded-md' }}">
-                <div class="grid grid-cols-6 gap-6">
+        <form class="m-auto" wire:submit.prevent="{{ $submit }}">
+            <div class="mb-6 md:items-center" {{ isset($actions) ? 'sm:rounded-tl-md sm:rounded-tr-md' : 'sm:rounded-md' }}">
+                <div class="flex flex-col sm:flex-row">
                     {{ $form }}
                 </div>
             </div>
 
             @if (isset($actions))
-                <div class="flex items-center justify-end px-4 py-3 bg-gray-50 text-right sm:px-6 shadow sm:rounded-bl-md sm:rounded-br-md">
+                <div class="flex items-center justify-end px-4 py-3 text-right sm:px-6 sm:rounded-bl-md sm:rounded-br-md">
                     {{ $actions }}
                 </div>
             @endif
