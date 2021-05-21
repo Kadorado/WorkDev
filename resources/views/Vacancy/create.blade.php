d2235a9a0839a349e4d2eefdc6f06f3fe9815200
Poly <rodriguezpaula606@gmail.com>
2021-05-21 09:37:05 -0500

    formulario nueva vacante

--- resources/views/Vacancy/create.blade.php
+++ resources/views/Vacancy/create.blade.php
@@ -8,144 +8,154 @@
 <h2>Crear Vacante</h2>
 
 
-
-<form action="/vacante" method="POST">
-    @csrf
-   
-    <div class="mb-3">
-        <label for="" class="form-label">Titulo</label>
-        <input type="text" name="title" id="title"  type="text" class="form-control" tabindex="1"  >
+<form action="/vacante" method="POST" autocomplete=”off”> 
+    @csrf               
+    <div class="relative w-full mb-3">
+        <small class="p-2 text-red-500">* Titulo</small>
+        <input type="text" name="title" id="title" tabindex="1"  class="w-full p-4 text-sm text-gray-700 placeholder-gray-400 bg-white border-0 rounded shadow focus:outline-none focus:ring" placeholder="Titulo de la vacante" style="transition: all 0.15s ease 0s;" required/>
     </div>
-    <div class="mb-3">
-        <label for="" class="form-label">Experiencia requerida</label>
-        <input type="text" name="experience" id="title"  type="number" class="form-control" tabindex="1"  >
-    </div> 
-    <div class="mb-3">
-        <label for="" class="form-label">Salario</label>
-        <input type="text" name="salary" id="title"  type="number" class="form-control" tabindex="1"  >
-    </div> 
-
-    <div class="mb-3">
-        <label for="" class="form-label">Lugar</label>
-        <input type="text" name="location" id="title"  type="texto" class="form-control" tabindex="1"  >
-    </div> 
-
-
-    <div class="mb-3">
-        <label for="" class="form-label">Moneda</label>
-        <input type="text" name="currency" id="title"  type="text" class="form-control" tabindex="1"  >
+    <div class="relative w-full mb-3">
+        <small class="p-2 text-red-500">* Experiencia requerida</small>
+        <input  name="experience" id="title"  type="number" tabindex="1" class="w-full p-4 text-sm text-gray-700 placeholder-gray-400 bg-white border-0 rounded shadow focus:outline-none focus:ring" placeholder="Experiencia requerida en meses" style="transition: all 0.15s ease 0s;" required/>
     </div>
+    <div class="relative w-full mb-3">
+        <small class="p-2 text-red-500">* Salario</small>
+        <input  name="salary" id="title"  type="number" tabindex="1" class="w-full p-4 text-sm text-gray-700 placeholder-gray-400 bg-white border-0 rounded shadow focus:outline-none focus:ring" placeholder="eje.200.000" style="transition: all 0.15s ease 0s;" required />
+    </div>
+    <div class="relative w-full mb-3">
+        <small class="p-2 text-red-500">* Lugar</small>
+        <input   type="text" name="location" id="title"  tabindex="1" class="w-full p-4 text-sm text-gray-700 placeholder-gray-400 bg-white border-0 rounded shadow focus:outline-none focus:ring" placeholder="eje.Colombia" style="transition: all 0.15s ease 0s;" required/>
+    </div>
+    <div>
+        <small class="p-2 text-red-500">* Moneda</small>
+        <div class="flex flex-col w-full h-64 mx-auto md:w-1/2">
+            <select name="currency" class="flex w-full p-4 text-sm text-gray-700 placeholder-gray-400 bg-white border-0 rounded shadow focus:outline-none focus:ring">
+                <div class="w-full bg-red-400 border-b border-gray-100 rounded-t cursor-pointer hover:bg-teal-100">
+                    
+                    <option value="USD" class="relative flex items-center w-full p-2 pl-2 border-l-2 border-transparent">USD</option>
+                    <option value="COP" class="relative flex items-center w-full p-2 pl-2 border-l-2 border-transparent">COP</option>
+                    <option value="EUR" class="relative flex items-center w-full p-2 pl-2 border-l-2 border-transparent">EUR</option>
+                </div>
+            </select>
+        </div>
+    </div>
+    <div class="relative w-full mb-3">
+        <small class="p-2 text-red-500">* Descripcion de la vacante</small>
+        <textarea name="descriptionjob" id="title" placeholder="eje.Desarrollador" cols="30" rows="10" tabindex="1" class="w-full p-4 text-sm text-gray-700 placeholder-gray-400 bg-white border-0 rounded shadow focus:outline-none focus:ring" required></textarea>
+    </div>
+    <div >
+        <small class="p-2 text-red-500">* Tecnologias requeridas</small>
+      <style>
+          [x-cloak] {
+              display: none;
+          }
+      </style>
+      <select x-cloak id="select">
+          @foreach ($tecnologies as $tecno)
+          <option value={{ $tecno->id}}>{{ $tecno->tecno}}</option>
+          @endforeach
+      </select>
+      <div x-data="dropdown()" x-init="loadOptions()" class="flex flex-col w-full h-64 mx-auto md:w-1/2">
+        <input name="values" type="hidden" x-bind:value="selectedValues()">
+        <div class="relative inline-block w-full">
+            <div class="relative flex flex-col items-center">
+                <div x-on:click="open" class="w-full svelte-1l8159u">
+                    <div class="flex w-full p-4 text-sm text-gray-700 placeholder-gray-400 bg-white border-0 rounded shadow focus:outline-none focus:ring">
+                        <div class="flex flex-wrap flex-auto">
+                            <template x-for="(option,index) in selected" :key="options[option].value">
+                                <div
+                                    class="flex items-center justify-center w-2/5 px-2 py-1 m-1 font-medium text-teal-700 bg-white bg-teal-100 border border-teal-300 rounded-full ">
+                                    <div class="text-xs font-normal leading-none max-w-full flex-initial x-model="
+                                        options[option]" x-text="options[option].text"></div>
+                                    <div class="flex flex-row-reverse flex-auto">
+                                        <div x-on:click="remove(index,option)">
+                                            <svg class="w-6 h-6 rounded-full fill-current hover:bg-red-200 " role="button" viewBox="0 0 20 20">
+                                                <path style="fill:#D1403F;" d="M14.348,14.849c-0.469,0.469-1.229,0.469-1.697,0L10,11.819l-2.651,3.029c-0.469,0.469-1.229,0.469-1.697,0
+                                           c-0.469-0.469-0.469-1.229,0-1.697l2.758-3.15L5.651,6.849c-0.469-0.469-0.469-1.228,0-1.697s1.228-0.469,1.697,0L10,8.183
+                                           l2.651-3.031c0.469-0.469,1.228-0.469,1.697,0s0.469,1.229,0,1.697l-2.758,3.152l2.758,3.15
+                                           C14.817,13.62,14.817,14.38,14.348,14.849z" />
+                                            </svg>
 
-    <div class="mb-3">
-        <label for="" class="form-label">Descripcion de la vacante</label>
-        <input type="text" name="descriptionjob" id="title"  type="number" class="form-control" tabindex="1"  >
-    </div> 
-        <div >
-            <label for="" class="form-label">Tecnologias requeridas</label>
-
-          <style>
-              [x-cloak] {
-                  display: none;
-              }
-          </style>
-          <select x-cloak id="select">
-              @foreach ($tecnologies as $tecno)
-              <option value={{ $tecno->id}}>{{ $tecno->tecno}}</option>
-              @endforeach
-          </select>
-          <div x-data="dropdown()" x-init="loadOptions()" class="w-full md:w-1/2 flex flex-col  h-64 mx-auto">
-            <input name="values" type="hidden" x-bind:value="selectedValues()">
-            <div class="inline-block relative w-64">
-                <div class="flex flex-col items-center relative">
-                    <div x-on:click="open" class="w-full  svelte-1l8159u">
-                        <div class="my-2 p-1 flex border border-gray-200 bg-white rounded svelte-1l8159u">
-                            <div class="flex flex-auto flex-wrap">
-                                <template x-for="(option,index) in selected" :key="options[option].value">
-                                    <div
-                                        class="flex justify-center items-center m-1 font-medium py-1 px-2 bg-white rounded-full text-teal-700 bg-teal-100 border border-teal-300 ">
-                                        <div class="text-xs font-normal leading-none max-w-full flex-initial x-model="
-                                            options[option]" x-text="options[option].text"></div>
-                                        <div class="flex flex-auto flex-row-reverse">
-                                            <div x-on:click="remove(index,option)">
-                                                <svg class="fill-current h-6 w-6 " role="button" viewBox="0 0 20 20">
-                                                    <path d="M14.348,14.849c-0.469,0.469-1.229,0.469-1.697,0L10,11.819l-2.651,3.029c-0.469,0.469-1.229,0.469-1.697,0
-                                               c-0.469-0.469-0.469-1.229,0-1.697l2.758-3.15L5.651,6.849c-0.469-0.469-0.469-1.228,0-1.697s1.228-0.469,1.697,0L10,8.183
-                                               l2.651-3.031c0.469-0.469,1.228-0.469,1.697,0s0.469,1.229,0,1.697l-2.758,3.152l2.758,3.15
-                                               C14.817,13.62,14.817,14.38,14.348,14.849z" />
-                                                </svg>
-    
-                                            </div>
                                         </div>
                                     </div>
-                                </template>
-                                <div x-show="selected.length    == 0" class="flex-1">
-                                    <input placeholder="Select a option"
-                                        class="bg-transparent p-1 px-2 appearance-none outline-none h-full w-full text-gray-800"
-                                        x-bind:value="selectedValues()"
-                                        name="userTecno" id="userTecno"
-                                    >
                                 </div>
-                            </div>
-                            <div
-                                class="text-gray-300 w-8 py-1 pl-2 pr-1 border-l flex items-center border-gray-200 svelte-1l8159u">
-    
-                                <button type="button" x-show="isOpen() === true" x-on:click="open"
-                                    class="cursor-pointer w-6 h-6 text-gray-600 outline-none focus:outline-none">
-                                    <svg version="1.1" class="fill-current h-4 w-4" viewBox="0 0 20 20">
-                                        <path d="M17.418,6.109c0.272-0.268,0.709-0.268,0.979,0s0.271,0.701,0,0.969l-7.908,7.83
-        c-0.27,0.268-0.707,0.268-0.979,0l-7.908-7.83c-0.27-0.268-0.27-0.701,0-0.969c0.271-0.268,0.709-0.268,0.979,0L10,13.25
-        L17.418,6.109z" />
-                                    </svg>
-    
-                                </button>
-                                <button type="button" x-show="isOpen() === false" @click="close"
-                                    class="cursor-pointer w-6 h-6 text-gray-600 outline-none focus:outline-none">
-                                    <svg class="fill-current h-4 w-4" viewBox="0 0 20 20">
-                                        <path d="M2.582,13.891c-0.272,0.268-0.709,0.268-0.979,0s-0.271-0.701,0-0.969l7.908-7.83
-        c0.27-0.268,0.707-0.268,0.979,0l7.908,7.83c0.27,0.268,0.27,0.701,0,0.969c-0.271,0.268-0.709,0.268-0.978,0L10,6.75L2.582,13.891z
-        " />
-                                    </svg>
-    
-                                </button>
+                            </template>
+                            <div x-show="selected.length    == 0" class="flex-1">
+                                <input placeholder="Select a option"
+                                    class="w-full h-full p-1 px-2 text-gray-800 bg-transparent outline-none appearance-none"
+                                    x-bind:value="selectedValues()"
+                                    name="userTecno" id="userTecno"
+                                >
                             </div>
                         </div>
-                    </div>
-                    <div class="w-full px-4">
-                        <div x-show.transition.origin.top="isOpen()"
-                            class="absolute shadow top-100 bg-white z-40 w-full lef-0 rounded max-h-select overflow-y-auto svelte-5uyqqj"
-                            x-on:click.away="close">
-                            <div class="flex flex-col w-full">
-                                <template x-for="(option,index) in options" :key="option">
-                                    <div>
-                                        <div class="cursor-pointer w-full border-gray-100 rounded-t border-b hover:bg-teal-100"
-                                            @click="select(index,$event)">
-                                            <div x-bind:class="option.selected ? 'border-teal-600' : ''"
-                                                class="flex w-full items-center p-2 pl-2 border-transparent border-l-2 relative">
-                                                <div class="w-full items-center flex">
-                                                    <div class="mx-2 leading-6" x-model="option" x-text="option.text"></div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </template>
-                            </div>
+                        <div
+                            class="flex items-center w-8 py-1 pl-2 pr-1 text-gray-300 border-l border-gray-200 svelte-1l8159u">
+
+                            <button type="button" x-show="isOpen() === true" x-on:click="open"
+                                class="w-6 h-6 text-gray-600 outline-none cursor-pointer focus:outline-none">
+                                <svg version="1.1" class="w-4 h-4 fill-current" viewBox="0 0 20 20">
+                                    <path d="M17.418,6.109c0.272-0.268,0.709-0.268,0.979,0s0.271,0.701,0,0.969l-7.908,7.83
+    c-0.27,0.268-0.707,0.268-0.979,0l-7.908-7.83c-0.27-0.268-0.27-0.701,0-0.969c0.271-0.268,0.709-0.268,0.979,0L10,13.25
+    L17.418,6.109z" />
+                                </svg>
+
+                            </button>
+                            <button type="button" x-show="isOpen() === false" @click="close"
+                                class="w-6 h-6 text-gray-600 outline-none cursor-pointer focus:outline-none">
+                                <svg class="w-4 h-4 fill-current" viewBox="0 0 20 20">
+                                    <path d="M2.582,13.891c-0.272,0.268-0.709,0.268-0.979,0s-0.271-0.701,0-0.969l7.908-7.83
+    c0.27-0.268,0.707-0.268,0.979,0l7.908,7.83c0.27,0.268,0.27,0.701,0,0.969c-0.271,0.268-0.709,0.268-0.978,0L10,6.75L2.582,13.891z
+    " />
+                                </svg>
+
+                            </button>
                         </div>
                     </div>
                 </div>
-                <!-- on tailwind components page will no work  -->
-            </form>
+                <div class="w-full px-4">
+                    <div x-show.transition.origin.top="isOpen()"
+                        class="absolute z-40 w-full overflow-y-auto bg-white rounded shadow top-100 lef-0 max-h-select svelte-5uyqqj"
+                        x-on:click.away="close">
+                        <div class="flex flex-col w-full">
+                            <template x-for="(option,index) in options" :key="option">
+                                <div>
+                                    <div class="w-full border-b border-gray-100 rounded-t cursor-pointer hover:bg-teal-100"
+                                        @click="select(index,$event)">
+                                        <div x-bind:class="option.selected ? 'border-teal-600' : ''"
+                                            class="relative flex items-center w-full p-2 pl-2 border-l-2 border-transparent">
+                                            <div class="flex items-center w-full">
+                                                <div class="mx-2 leading-6" x-model="option" x-text="option.text"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </template>
+                        </div>
+                    </div>
+                </div>
             </div>
-      </div>
+        
+        </div>
+    </div>
+  
+    {{-- estado --}}
+    <div>
+        <small class="p-2 text-red-500">* Estado</small>
+        <div class="flex flex-col w-full h-64 mx-auto md:w-1/2">
+            <select  name="state" class="flex w-full p-4 text-sm text-gray-700 placeholder-gray-400 bg-white border-0 rounded shadow focus:outline-none focus:ring">
+                <div class="w-full bg-red-400 border-b border-gray-100 rounded-t cursor-pointer hover:bg-teal-100">
+                    <option value="0" class="relative flex items-center w-full p-2 pl-2 border-l-2 border-transparent">Abierto</option>
+                    <option value="1" class="relative flex items-center w-full p-2 pl-2 border-l-2 border-transparent">Cerrado</option>
+                </div>
+            </select>
+        </div>
+    </div>
 
-    <div class="mb-3">
-        <label for="" class="form-label">estado</label>
-        <input type="text" name="state" id="title"  type="texto" class="form-control" tabindex="1"  >
-    </div> 
-
-    <a href="/vacante" class="btn btn-secondary" tabindex="">Cancelar</a>
-    <button type="submit" class="btn btn-primary">Guardar</button>
+    <div class="mt-6 text-center">
+        <button type="submit" class="justify-center w-32 p-3 text-white bg-green-600 rounded-lg shadow outline-none focus:bg-green-700 hover:bg-green-500">Guardar</button>
+    </div>  
+    
 </form>
-
 
 @stop
 
